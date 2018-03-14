84f17551dc3ba4037aef3a6dcac747e0a64b3a3f
Jamie Bort <jamiebort@gmail.com>
2018-03-14 13:02:11 -0400

    Updated index.html and main.js for picture.

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-fetch("https://api.github.com/users/jamiebort")
+// let request = new XMLHttpRequest();
+// request.addEventListener("load", displayGitHubInfo);
+// request.open('GET', 'https://api.github.com/users/jamiebort');
+// request.send();
+
+// function displayGitHubInfo() {
+//   let data = JSON.parse(this.responseText);
+//   document.getElementById('name').innerHTML=`Name: ` + data.name;
+//   document.getElementById('url').innerHTML=`URL: ` + data.url;
+//   document.getElementById('email').innerHTML=`Email: ` + data.email;
+//   document.getElementById('company').innerHTML=`Company: ` + data.company;
+//   document.getElementById('blog').innerHTML=`Website: ` + data.blog;
+//   document.getElementById('bio').innerHTML=data.bio;
+// };
+
+fetch('https://api.github.com/users/jamiebort')
   // Data is fetched and we get a promise.
   .then(
     // The promise returns a response from the server.
@@ -9,13 +24,17 @@ fetch("https://api.github.com/users/jamiebort")
         return;
       }
       response.json().then(function(data) {
-        // console.log("Here is the data:", data);
-        document.getElementById('name').innerHTML = `Name: ` + data.name;
-        document.getElementById('url').innerHTML = `URL: ` + data.url;
-        document.getElementById('email').innerHTML = `Email: ` + data.email;
-        document.getElementById('company').innerHTML = `Company: ` + data.company;
-        document.getElementById('blog').innerHTML = `Website: ` + data.blog;
-        document.getElementById('bio').innerHTML = data.bio;
+
+        // let data = JSON.parse(this.responseText);
+  document.getElementById('name').innerHTML=`Name: ` + data.name;
+  document.getElementById('url').innerHTML=`URL: ` + data.url;
+  document.getElementById('email').innerHTML=`Email: ` + data.email;
+  document.getElementById('company').innerHTML=`Company: ` + data.company;
+  document.getElementById('blog').innerHTML=`Website: ` + data.blog;
+  document.getElementById('bio').innerHTML=data.bio;
+  var pic = document.getElementById('pic')
+  pic.setAttribute("src", data.avatar_url);
+        console.log("Here is the data:", data);
       });
     }
   )
