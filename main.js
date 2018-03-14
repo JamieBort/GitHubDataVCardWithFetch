c8480962141f25ac0561a4d92130b35b47777038
Jamie Bort <jamiebort@gmail.com>
2018-03-14 13:17:48 -0400

    Updated index.html and main.js for picture.

--- main.js
+++ main.js
@@ -1,43 +1,27 @@
-// let request = new XMLHttpRequest();
-// request.addEventListener("load", displayGitHubInfo);
-// request.open('GET', 'https://api.github.com/users/jamiebort');
-// request.send();
-
-// function displayGitHubInfo() {
-//   let data = JSON.parse(this.responseText);
-//   document.getElementById('name').innerHTML=`Name: ` + data.name;
-//   document.getElementById('url').innerHTML=`URL: ` + data.url;
-//   document.getElementById('email').innerHTML=`Email: ` + data.email;
-//   document.getElementById('company').innerHTML=`Company: ` + data.company;
-//   document.getElementById('blog').innerHTML=`Website: ` + data.blog;
-//   document.getElementById('bio').innerHTML=data.bio;
-// };
-
 fetch('https://api.github.com/users/jamiebort')
   // Data is fetched and we get a promise.
   .then(
     // The promise returns a response from the server.
-    function(response) {
+    function (response) {
       // We process the response accordingly.
       if (response.status !== 200) {
         console.log(response.status);
         return;
       }
-      response.json().then(function(data) {
+      response.json().then(function (data) {
 
-        // let data = JSON.parse(this.responseText);
-  document.getElementById('name').innerHTML=`Name: ` + data.name;
-  document.getElementById('url').innerHTML=`URL: ` + data.url;
-  document.getElementById('email').innerHTML=`Email: ` + data.email;
-  document.getElementById('company').innerHTML=`Company: ` + data.company;
-  document.getElementById('blog').innerHTML=`Website: ` + data.blog;
-  document.getElementById('bio').innerHTML=data.bio;
-  var pic = document.getElementById('pic')
-  pic.setAttribute("src", data.avatar_url);
+        document.getElementById('name').innerHTML = `Name: ` + data.name;
+        document.getElementById('url').innerHTML = `URL: ` + data.url;
+        document.getElementById('email').innerHTML = `Email: ` + data.email;
+        document.getElementById('company').innerHTML = `Company: ` + data.company;
+        document.getElementById('blog').innerHTML = `Website: ` + data.blog;
+        document.getElementById('bio').innerHTML = data.bio;
+        var pic = document.getElementById('pic')
+        pic.setAttribute("src", data.avatar_url);
         console.log("Here is the data:", data);
       });
     }
   )
-  .catch(function(err) {
+  .catch(function (err) {
     console.log("Fetch Error :-S", err);
   });
